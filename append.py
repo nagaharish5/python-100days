da688dfc70834f7fee64050f9dd0fe7c723b9c2f
Harish <nagaharish596@gmail.com>
2026-03-01 02:09:07 +0530

    Create append.py

---
+++ append.py
@@ -0,0 +1,9 @@
+#https://docs.python.org/3/tutorial/datastructures.html
+
+state1=["telangana"]
+state2=["andhra"]
+states_of_india=["kerala","andhra","telangana"]
+
+print(states_of_india[-1])
+states_of_india.append("India")
+print(states_of_india)
