a54626b966f8350e528dbac99312e101d907347a
Harish <nagaharish596@gmail.com>
2026-03-01 02:04:11 +0530

    Create rollercoaster-ride.py

---
+++ ifelse/rollercoaster-ride.py
@@ -0,0 +1,7 @@
+print("welcome to roller coster ride")
+height=int(input("please enter your height in m"))
+
+if height >=120:
+    print("you can ride the rollercoster ride")
+else:
+     print("you cannot ride the rollercoster ride")
