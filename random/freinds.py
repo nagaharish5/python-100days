1648324e2976e42a03336ef7254a09a43b69ae2c
Harish <nagaharish596@gmail.com>
2026-03-01 02:06:37 +0530

    Create freinds.py

---
+++ random/freinds.py
@@ -0,0 +1,10 @@
+import random
+
+freinds=["harish","hari","Bablu"]
+#1 st option
+print(freinds)
+print (random.choice(freinds))
+
+#2nd option
+random_index =random.randint(0,2)
+print(freinds[random_index])
