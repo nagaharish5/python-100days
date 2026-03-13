e9b2e3540f7b349ffaa7a8e562c66b4da1f238e4
Harish <nagaharish596@gmail.com>
2026-03-13 23:14:10 +0530

    Create for-loop1.py

---
+++ loops/forloop.py
@@ -0,0 +1,20 @@
+#gemini
+# print("hello world")
+# print("This is your new password")
+#
+# # Separating the strings so the loop actually iterates three times
+# fruits = ["apple", "pears", "orange"]
+#
+# for fruit in fruits:
+#     print(fruit)
+#     # Using 'fruit' (singular) and adding a space for readability
+#     print(fruit + " mango")
+
+
+#fruits=["apple,pears,orage"]
+fruits=["apple","pears","orage"]
+for fruit in fruits:
+    print (fruit)
+    print(fruit+ " mango")
+    print(fruits)
+print(fruits)
